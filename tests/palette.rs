38360802ab554c220fa7ccf6a04d14d61a00edb4
johanyim <37012949+johanyim@users.noreply.github.com>
2023-11-17 23:40:36 +0000

    tests

--- tests/palette.rs
+++ tests/palette.rs
@@ -13,12 +13,12 @@ mod tests {
         let mut testpal = Palette::new("testpal");
         let black = "000000";
 
-        let _ = testpal.add('q', "green", color::Format::HEX, "rgb(0,255,0)"); 
-        let _ = testpal.add('w', "cyan", color::Format::HEXA, "00ffff"); 
-        let _ = testpal.add('e', "magenta", color::Format::RGB, "magenta"); 
-        let _ = testpal.add('r', "red", color::Format::RGBA, "ff000088"); 
-        let _ = testpal.add('t', "glass", color::Format::RGBA, "transparent"); 
-        let _ = testpal.add('y', "black", color::Format::HEX, black); 
+        let _ = testpal.set('q', "green", color::Format::HEX, "rgb(0,255,0)"); 
+        let _ = testpal.set('w', "cyan", color::Format::HEXA, "00ffff"); 
+        let _ = testpal.set('e', "magenta", color::Format::RGB, "magenta"); 
+        let _ = testpal.set('r', "red", color::Format::RGBA, "ff000088"); 
+        let _ = testpal.set('t', "glass", color::Format::RGBA, "transparent"); 
+        let _ = testpal.set('y', "black", color::Format::HEX, black); 
 
         assert_eq!(testpal.get_string('q'), "#00ff00");
         assert_eq!(testpal.get_string('w'), "#00ffffff");
@@ -31,7 +31,7 @@ mod tests {
     #[test]
     fn removes_colors() {
         let mut testpal = Palette::new("testpal");
-        let _ = testpal.add('w', "grey", color::Format::HEXA, "rgba(128,128,128,0.01)"); 
+        let _ = testpal.set('w', "grey", color::Format::HEXA, "rgba(128,128,128,0.01)"); 
         
         //testing that te color has been added
         assert_eq!(testpal.get_string('w'), "#80808003");
@@ -41,14 +41,14 @@ mod tests {
         assert_eq!(removed.unwrap().to_string(), "#80808003");
 
         // color assigned to the key is now empty/black
-        assert_eq!(testpal.get_name('w'), "Unassigned");        
+        assert_eq!(testpal.get_name('w'), "");        
         assert_eq!(testpal.get_string('w'), "#000000");        
     }
 
     #[test]
     fn colors_are_not_borrowed() {
         let mut testpal = Palette::new("testpal");
-        let _ = testpal.add('q', "blue", color::Format::HEX, "rgb(0,0,255)"); 
+        let _ = testpal.set('q', "blue", color::Format::HEX, "rgb(0,0,255)"); 
         assert_eq!(testpal.get_string('q'), "#0000ff");
         assert_eq!(testpal.get_string('q'), "#0000ff");
         assert_eq!(testpal.get_string('q'), "#0000ff");
@@ -58,7 +58,7 @@ mod tests {
     fn adding_invalid_color() {
         let invalid_color = "#222aldskjagasdh".to_string();
         let mut testpal = Palette::new("testpal");
-        testpal.add('q', "an invalid color", 
+        testpal.set('q', "an invalid color", 
                             color::Format::RGBA, 
                             &invalid_color);
         
@@ -68,7 +68,7 @@ mod tests {
 
         // color will not be assigned
         assert!(testpal.colors.get(&'q').is_none());
-        assert_eq!(testpal.get_name('q'), "Unassigned");
+        assert_eq!(testpal.get_name('q'), "");
         assert_eq!(testpal.get_string('q'), "#000000");
     }
 
@@ -78,14 +78,14 @@ mod tests {
         let invalid_color = "#FFalFkjagasdh".to_string();
         let mut testpal = Palette::new("testpal");
 
-        testpal.add('q', "yellow", 
+        testpal.set('q', "yellow", 
                             color::Format::HEXA, 
                             &valid_color);
         
         assert_eq!(testpal.get_name('q'), "yellow");
         assert_eq!(testpal.get_string('q'), "#ffff00ff");
 
-        testpal.add('q', "invalid", 
+        testpal.set('q', "invalid", 
                             color::Format::RGB, 
                             &invalid_color);
 
@@ -99,7 +99,7 @@ mod tests {
     #[test]
     fn changes_output_format() {
         let mut testpal = Palette::new("testpal");
-        testpal.add('q', "transparent yellow", 
+        testpal.set('q', "transparent yellow", 
                             color::Format::HEX, 
                             "#ffff0080");
         // color no longer shows alpha channel, though it is still remembered
@@ -112,7 +112,7 @@ mod tests {
     #[test]
     fn cmyk_format() {
         let mut testpal = Palette::new("testpal");
-        testpal.add('q', 
+        testpal.set('q', 
             "navy",
             color::Format::CMYK,
             "#2D4192"
@@ -124,49 +124,49 @@ mod tests {
     fn hsl_format() {
         let mut testpal = Palette::new("testpal");
 
-        testpal.add('q', 
+        testpal.set('q', 
             "brown",
             color::Format::HSL,
             "rgb(100,21,12)"
             ); 
         assert_eq!(testpal.get_string('q'), "hsl(6deg 79% 22%)");
 
-        testpal.add('w', 
+        testpal.set('w', 
             "transparent navy",
             color::Format::HSL,
             "#03044f45"
             ); 
         assert_eq!(testpal.get_string('w'), "hsl(239deg 93% 16% / 0.27)");
 
-        testpal.add('e', 
+        testpal.set('e', 
             "opaque green",
             color::Format::HSL,
             "#00ff00ff"
             ); 
         assert_eq!(testpal.get_string('e'), "hsl(120deg 100% 50%)");
 
-        testpal.add('r', 
+        testpal.set('r', 
             "half transparent blue",
             color::Format::HSL,
             "#0000ff80"
             ); 
         assert_eq!(testpal.get_string('r'), "hsl(240deg 100% 50% / 0.50)");
 
-        testpal.add('t', 
+        testpal.set('t', 
             "opaque white",
             color::Format::HSL,
             "rgb(255,255,255)"
             ); 
         assert_eq!(testpal.get_string('t'), "hsl(0deg 0% 100%)");
 
-        testpal.add('y', 
+        testpal.set('y', 
             "opaque grey",
             color::Format::HSL,
             "rgb(128,128,128)"
             ); 
         assert_eq!(testpal.get_string('y'), "hsl(0deg 0% 50%)");
 
-        testpal.add('u', 
+        testpal.set('u', 
             "opaque black",
             color::Format::HSL,
             "rgb(0,0,0)"
@@ -178,7 +178,7 @@ mod tests {
     fn deletes_alpha() {
         let mut testpal = Palette::new("testpal");
 
-        testpal.add('q', 
+        testpal.set('q', 
             "blue",
             color::Format::HEXA,
             "rgba(0,0,255,0.3)"
